615e08f6de98548cad2ad4df315c06a23becf33f
sara <sarah_khoshaghebat@yahoo.com>
2021-09-29 21:36:25 +0330

    add file

---
+++ src/cooche-background-green.js
@@ -0,0 +1,12 @@
+import react from "react";
+
+
+class BackgroundGreen extends react.Component {
+    render() {
+        return (
+            <div className="background-img-green">
+			    <span class="text-green">کوچه‌ها رو با هم بگردیم</span>
+		    </div>
+        );
+    }
+}

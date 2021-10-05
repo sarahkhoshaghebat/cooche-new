68a14f3842e9507341c1f7332a483348d2bae496
sara <sarah_khoshaghebat@yahoo.com>
2021-10-05 14:47:46 +0330

    redesign

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
-import react from 'react';
+import React ,{ Component } from 'react';
 import './app.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route,Link,BrowserRouter as router } from 'react-router-dom';
 import TextCenter from './text-center';
 import Pc from './pc'
 import Neighbourhood from './neighbourhood-text';
@@ -8,9 +9,11 @@ import BackgroundGreen from './cooche-background-green';
 import Privacy from './privacy ';
 import Footer from './footer';
 import Register from './register'
+import { Form } from 'react-bootstrap';
+import logo from './img/logo.png';
 
 
-class MenuBar extends react.Component {
+class MenuBar extends Component {
     render () {
         return (
 			<>
@@ -18,11 +21,11 @@ class MenuBar extends react.Component {
 		   			 <div class="row mb-4">
 						<div class="col-6 col-sm-6">
 							<button type="button" class="login ml-md-4">ورود/ثبت نام</button>
-							<button type="button" onClick={Register()} class="Download">دانلود</button>
+							<button type="button" onClick={<Register/>} class="Download">دانلود</button>
 						</div>
 						<div className="col-6 col-sm-6">
 							<a href="/">
-								<img src="/logo.png" className="img-logo mr-md-5" alt="coocheh"/>
+								<img src={logo} className="img-logo mr-md-5" alt="coocheh"/>
 							</a>
 			   			 </div>
 		    		</div>

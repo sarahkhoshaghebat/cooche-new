d13951f1ef9c10c0be26a7026e5b08995ef5dc91
sara <sarah_khoshaghebat@yahoo.com>
2021-10-09 17:23:15 +0330

    redesign

--- src/app.js
+++ src/app.js
@@ -8,16 +8,18 @@ import BackgroundGreen from './cooche-background-green';
 import Privacy from './privacy ';
 import Footer from './footer';
 import logo from './img/logo.png';
+import Register from './register'
+import reactDom from 'react-dom';
 
 
 class MenuBar extends Component {
     render () {
         return (
-			<>
+			<React.Fragment>
         		<div className="container-fluid menu">
 		   			<div className="row mb-4">
 						<div className="col-6 col-sm-6">
-							<button type="button" data-bs-toggle="register" data-bs-target="#myRegister" className="login ml-md-4">ورود/ثبت نام</button>
+							<button type="button" className="login ml-md-4">ورود/ثبت نام</button>
 							<button type="button" className="Download">دانلود</button>
 						</div>
 						<div className="col-6 col-sm-6">
@@ -33,7 +35,7 @@ class MenuBar extends Component {
 				<BackgroundGreen/>
 				<Privacy/>
 				<Footer/>
-    		</>
+    		</React.Fragment>
 		)
 	}
 }
--- src/register.js
+++ src/register.js
@@ -7,11 +7,24 @@ import './app.css';
 class Register extends Component {
     render() {
         return(
-           <div className="modal">
-               <div className="modal-dialog">
-                    <img src={logo} />
-               </div>
-           </div>
+            <div class="modal" tabindex="-1">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title">Modal title</h5>
+                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                  <p>Modal body text goes here.</p>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                  <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          
         );
     }
 }

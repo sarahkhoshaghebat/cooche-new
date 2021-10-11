65090a923018910551be48e17eb4479c0fba1113
sara <sarah_khoshaghebat@yahoo.com>
2021-10-11 17:52:35 +0330

    redesign

--- src/app.js
+++ src/app.js
@@ -8,7 +8,6 @@ import BackgroundGreen from './cooche-background-green';
 import Privacy from './privacy ';
 import Footer from './footer';
 import logo from './img/logo.png';
-import Register from './register'
 import reactDom from 'react-dom';
 
 
@@ -29,30 +28,6 @@ class MenuBar extends Component {
 			   			</div>
 		    		</div>
 	    		</div>
-
-
-				<button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-				Launch demo modal
-				</button>
-
-
-				<div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-				<div className="modal-dialog">
-					<div className="modal-content">
-					<div className="modal-header">
-						<h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
-						<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-					</div>
-					<div className="modal-body">
-						...
-					</div>
-					<div className="modal-footer">
-						<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-						<button type="button" className="btn btn-primary">Save changes</button>
-					</div>
-					</div>
-				</div>
-				</div>
 				<TextCenter/>
 				<Pc/>
 				<Neighbourhood/>
